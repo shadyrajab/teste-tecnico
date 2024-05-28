7c795d1d26d05cd3fc4051994e60db521f8ee20c
shady <shadyrajaab@gmail.com>
2024-05-28 16:38:30 -0300

    feat: adicioanar testes end to end

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { ProducersModule } from '../src/modules/producers.module';
+import { typeOrmConfig } from '../src/configs/typeorm.config';
+
+const producerData = {
+  "cnpj": "85262900100",
+  "produtor": "Shady Rajab",
+  "fazenda": "Fazenda Boa Vista",
+  "cidade": "Ribeirão Preto",
+  "estado": "SP",
+  "areaAgricultavel": 800.3,
+  "areaVegetacao": 200.2,
+  "areaFazenda": 1000.5,
+  "cultura": ["Soja", "Milho"]
+}
+
+const updateData = {
+  "fazenda": "Fazendinha Feliz",
+  "estado": "DF",
+  "cultura": ["Soja"]
+}
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        TypeOrmModule.forRoot(typeOrmConfig),
+        ProducersModule,
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -16,21 +39,29 @@ describe('AppController (e2e)', () => {
   });
 
   it('/producers (POST)', () => {
-    const producerData = {
-      "cnpj": "85262900100",
-      "produtor": "Shady Rajab",
-      "fazenda": "Fazenda Boa Vista",
-      "cidade": "Ribeirão Preto",
-      "estado": "SP",
-      "areaAgricultavel": 800.3,
-      "areaVegetacao": 200.2,
-      "areaFazenda": 1000.5,
-      "cultura": ["Soja", "Milho"]
-    }
     return request(app.getHttpServer())
       .post('/producers')
       .send(producerData)
+      .expect(201)
+  });
+
+  it('/producers:cnpj (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/producers/85262900100')
       .expect(200)
-      .expect({'message': "O CNPJ informado já existe na base de dados"});
+  });
+
+  it('/producers:cnpj (PUT)', () => {
+    return request(app.getHttpServer())
+      .put('/producers/85262900100')
+      .send(updateData)
+      .expect(200)
+  });
+
+  it('/producers (DELETE)', () => {
+    return request(app.getHttpServer())
+      .delete('/producers')
+      .send({ cnpj: "85262900100" })
+      .expect(200)
   });
 });
